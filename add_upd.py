461227bcc4786197b03e137a8d61c46f4d2a96d9
walidnaili <walidnaili998@gmail.com>
2025-01-23 02:41:35 +0100

    some_update

--- add_upd.py
+++ add_upd.py
@@ -1,19 +1,19 @@
-def Add_participant (data , name , date , score ) :
+def Add_participant (data , name , date ,category, score ) :
 
     new_participant = {
 
         "Name" : name,
-        "Participations" : [{"Date": date , "Score" : score}]
+        "Participations" : [{"Date": date ,"Category" : category ,"Score" : score}]
     }
 
     data.append(new_participant)
 
     return data
 
-def Update_participant (data ,name  , date , score ) :
+def Update_participant (data ,name  , date ,category, score ) :
     for participant in data :
         if participant["Name"] == name :
-            participant["Participations"].append({"Date": date , "Score" : score})
+            participant["Participations"].append({"Date": date ,"Category" : category, "Score" : score})
             return data 
 
     return data
